6c7c70bd718892dd9116381976632dd925e30e15
plabrador <pedroantonio.labrador@ezentis.com>
2020-04-06 12:24:33 +0200

    Initial version of shop generated by JHipster-6.8.0

---
+++ src/main/webapp/app/admin/health/health.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ShopSharedModule } from 'app/shared/shared.module';
+
+import { HealthComponent } from './health.component';
+import { HealthModalComponent } from './health-modal.component';
+
+import { healthRoute } from './health.route';
+
+@NgModule({
+  imports: [ShopSharedModule, RouterModule.forChild([healthRoute])],
+  declarations: [HealthComponent, HealthModalComponent],
+  entryComponents: [HealthModalComponent]
+})
+export class HealthModule {}
---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { ShopSharedLibsModule } from './shared-libs.module';
+import { FindLanguageFromKeyPipe } from './language/find-language-from-key.pipe';
+import { AlertComponent } from './alert/alert.component';
+import { AlertErrorComponent } from './alert/alert-error.component';
+import { LoginModalComponent } from './login/login.component';
+import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
+
+@NgModule({
+  imports: [ShopSharedLibsModule],
+  declarations: [FindLanguageFromKeyPipe, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
+  entryComponents: [LoginModalComponent],
+  exports: [
+    ShopSharedLibsModule,
+    FindLanguageFromKeyPipe,
+    AlertComponent,
+    AlertErrorComponent,
+    LoginModalComponent,
+    HasAnyAuthorityDirective
+  ]
+})
+export class ShopSharedModule {}
